3771dcb50f32bc609ab1711d7715ba981262d4aa
Taras J. Honcharuk <taras.honcharuk@gmail.com>
2016-02-25 23:05:32 +0200

    remote commit

--- src/bs01.erl
+++ src/bs01.erl
@@ -2,6 +2,7 @@
 -export([first_word/1]).
 
 first_word(Bintext)->first_word(Bintext,<<>>).
+%%
 
 first_word(<<" ",_/binary>>,Word)->Word;
 first_word(<<X,Bintext/binary>>,Word)->first_word(Bintext,<<Word/binary,X>>).
